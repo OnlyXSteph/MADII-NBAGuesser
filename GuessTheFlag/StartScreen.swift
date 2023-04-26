43c2a28038d30b86520e513f3f741c008abb4667
Stephanie Salgado <officialonlyxsteph@outlook.com>
2023-04-26 11:06:10 -0500

    Start

---
+++ GuessTheFlag/StartScreen.swift
@@ -0,0 +1,63 @@
+//
+//  StartScreen.swift
+//
+//  Created by Stephanie Salgado on 4/26/23.
+//
+
+import SwiftUI
+
+struct StartScreen: View {
+    @State var animate: Bool = false
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Image("StartImg")
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    Text("Let's Play!")
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.all)
+                        .border(Color.white, width: 5)
+                        .font(.system(size: 70))
+                        .shadow(color: Color.black,
+                                radius: 4, x: 6.5 , y: 6.5)
+                    
+                    Divider()
+                    
+                    NavigationLink(destination: ContentView(), label: {
+                        Image(systemName: "play.circle")
+                            .padding(.top)
+                            .font(.system(size:160))
+                            .foregroundColor(Color.white)
+                            .shadow(color: Color.black, radius: animate ? 30:10, x: 6.5, y: 6.5)
+                            .scaleEffect(animate ? 1.1 : 0.5)
+                            .offset(y: animate ? -7 : 0)
+                    }) .padding(.horizontal, animate ? 30:70)
+                }.onAppear(perform: addAnimation)
+            }
+        }
+    }
+    
+    func addAnimation(){
+        guard !animate else {
+            return
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            withAnimation(Animation .easeInOut(duration: 2.0) .repeatForever())
+            {
+                animate.toggle()
+            }
+        }
+    }
+}
+
+struct StartScreen_Previews: PreviewProvider {
+    static var previews: some View{
+        StartScreen()
+    }
+}
+                                      
+
